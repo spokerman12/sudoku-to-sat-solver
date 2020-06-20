8033b3cdc2eb802b318daa3dde58dfe7adbb0767
spokerman12 <spokerman12@hotmail.com>
2020-06-20 12:22:10 -0400

    Prints sudokus with color

--- script.py
+++ script.py
@@ -3,6 +3,26 @@ import os
 
 import numpy as np
 
+from termcolor import colored
+
+
+
+def print_sudoku(sudoku, order=3):
+	for i in range(order**2):
+		for j in range(order**2):
+			if (i//order)%2 == 0:
+				if (j//order)%2 == 0:
+					print(colored(sudoku[i,j],'cyan'),' ', end='')
+				else:
+					print(colored(sudoku[i,j],'yellow'),' ', end='')
+			if (i//order)%2 != 0:
+				if (j//order)%2 == 0:
+					print(colored(sudoku[i,j],'yellow'),' ', end='')
+				else:
+					print(colored(sudoku[i,j],'cyan'),' ', end='')
+		print('\n',end='') 
+	print('')
+
 if __name__ == '__main__':
 	if len(sys.argv) != 1:
 		print ("Loading file %s" % (sys.argv[1]))
@@ -16,4 +36,9 @@ if __name__ == '__main__':
 
 
 	for line in file.readlines():
-		print(line)
+		if line and line[0].isalnum():
+			# Mind this if we implement order 6 sudokus
+			# order = line[0]
+			read_line = list(line[2:].strip('\n'))
+			sudoku = np.array(np.array_split(read_line,9)).reshape((9,9))
+			print_sudoku(sudoku)
